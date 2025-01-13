0b1ad85cffae1bfe36db751715ac5cd4ea8eb720
Kamanpreet Singh <kskaman@gmail.com>
2025-01-13 15:50:12 -0330

    added custom colors and font families

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 export default {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ravenBlack: "#151619",
+        onyxGray: "#1d1f22",
+        charcoalGray: "282d31",
+        slateGray: "#35393f",
+
+        steelGray: "#5a6069",
+        pewterGray: "#7c8187",
+        mistGray: "c1c4cb",
+        silverMist: "#e4e4e4",
+
+        cloudWhite: "#f5f5f5",
+        pureWhite: "#ffffff",
+        coralOrange: "#e46643",
+        peachSunset: "#f39765",
+      },
+      fontFamily: {
+        sans: ["Roboto", "sans-serif"],
+        mono: ["Roboto Mono", "monospace"],
+        slab: ["Roboto Slab", "serif"],
+      },
+    },
   },
   plugins: [],
 };
